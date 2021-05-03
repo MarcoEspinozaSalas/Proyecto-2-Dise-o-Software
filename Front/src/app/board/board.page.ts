826fa10d9a1c463ac7c5d73b698b6d5d9703dd5f
Alejandro <moreraalejandro12@gmail.com>
2021-05-02 19:48:04 -0600

    Arreglo de pintar en el tablero

--- Front/src/app/board/board.page.ts
+++ Front/src/app/board/board.page.ts
@@ -1,31 +1,22 @@
 import { Component, OnInit, AfterViewInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
 import {Router} from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
-import { BoardPageRoutingModule } from './board-routing.module';
-
 import { HttpClient } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
+
 //services
 import { OthelloService } from './../services/othello.service';
 
 //models
-import { infoNewGame } from './../models/infoNewGame';
-import { addPlayerModal } from './../models/addPlayerModal';
-import { observable } from 'rxjs';
 import { editGame } from './../models/editGame';
-import { Button } from 'selenium-webdriver';
 import { skipTurn } from './../models/skpTurn'
-
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.page.html',
   styleUrls: ['./board.page.scss'],
 })
-export class BoardPage implements AfterViewInit {
+export class BoardPage implements AfterViewInit, OnInit {
 
   editGameWJugada = new editGame();
   skipTurn = new skipTurn();
@@ -38,7 +29,6 @@ export class BoardPage implements AfterViewInit {
   sc1:0;
   sc2:0;
   game: any;
-  
   playerTurn=1;
 
   constructor(private router: Router, private firebaseService: FirebaseService,public navCtrl: NavController, public http: HttpClient,
@@ -58,59 +48,65 @@ export class BoardPage implements AfterViewInit {
      this.router.navigate(['/login'])
     }
   }
+  ngOnInit(){
+    var card1 = document.getElementById("pt1") as HTMLInputElement;
+    card1.classList.add("pt1");
+    var card2 = document.getElementById("pt2") as HTMLInputElement;
+    card2.classList.remove("pt2");
+  }
 
   ngAfterViewInit() {
   }
 
   paint(c:number,p:string){
-    
     if(p!=null){
-      console.log(c+'-'+p);
       if (p=='X') {
+        //console.log('primer jugador turno');
         var buttonI = document.getElementById("button" + c.toString()) as HTMLInputElement;
-        console.log(buttonI.id);
-        
         buttonI.classList.add("mostrarN");
       }else if(p=='O'){
+        //console.log('segundo juagdor turno');
         var buttonA = document.getElementById("button" + c.toString()) as HTMLInputElement;
-        buttonA.classList.add("mostrar");
+        buttonA.classList.add("mostrar")
       }
-      
     }
-    
   }
   
 
 
   jugada(index:number){
+   
+    console.log(this.playerTurn);
     
     let turn;
-    if(this.playerTurn==1){
-
+    if(this.playerTurn==1){    
+      console.log(this.playerTurn);
+      
       var card1 = document.getElementById("pt1") as HTMLInputElement;
       card1.classList.add("pt1");
       var card2 = document.getElementById("pt2") as HTMLInputElement;
       card2.classList.remove("pt1");
+
       turn='X';
       this.playerTurn = 0;
+      console.log(this.playerTurn);
+      
       this.refresh(turn,index);
 
-      var buttonI = document.getElementById("button" + index.toString()) as HTMLInputElement;
-      buttonI.classList.add("mostrarN");
     }else if(this.playerTurn==0){
+      console.log(this.playerTurn);
+      
       var card3 = document.getElementById("pt2") as HTMLInputElement;
       card3.classList.add("pt1");
       var card4 = document.getElementById("pt1") as HTMLInputElement;
       card4.classList.remove("pt1");
+
       turn = 'O';
       this.playerTurn = 1;
-     
+      console.log(this.playerTurn);
       this.refresh(turn,index);
-
-      var buttonI = document.getElementById("button" + index.toString()) as HTMLInputElement;
-      buttonI.classList.add("mostrar");
     }
-    else{console.log('--------------------------------------------------------');
+    else{console.log('--------------------------8------------------------------');
     }
 
     
@@ -119,17 +115,6 @@ export class BoardPage implements AfterViewInit {
   }
 
   refresh(turn:string, index:number){
-    
-
-    this.othello.enterGame(this.gameId)
-        .subscribe( ( data:any ) => {
-          this.game = data.game;
-          this.secondPlayer = this.game.player2.playerName;
-          this.esto2 = this.game.boardGame;
-          this.sc1 = this.game.score.player1;
-          this.sc2 = this.game.score.player2;
-    });
-
     this.editGameWJugada.boardGame = this.esto2;
     this.editGameWJugada.currentPlayer = this.datosUsuarioLoggedIn.user.uid;
     this.editGameWJugada.idGame = this.gameId;
@@ -141,7 +126,6 @@ export class BoardPage implements AfterViewInit {
         console.log(data);
       }
     );
-
     this.othello.enterGame(this.gameId)
         .subscribe( ( data:any ) => {
           this.game = data.game;
@@ -150,15 +134,8 @@ export class BoardPage implements AfterViewInit {
           this.sc1 = this.game.score.player1;
           this.sc2 = this.game.score.player2;
     });
-
-    
-    
   }
 
 
-  setVar(){
-    this.datosUsuarioLoggedIn = JSON.parse(localStorage.getItem('user'));
-    this.othello.getGame(this.datosUsuarioLoggedIn.user.uid)
-  }
 
 }
